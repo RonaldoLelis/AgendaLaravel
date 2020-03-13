7ea31c3b907b517989a67253e6af98cb7a0d8a8a
Ronaldo de Lélis <delelis.locutor@gmail.com>
2020-03-12 23:27:57 -0300

    criando Salvar telefone

--- app/Http/Controllers/PessoasController.php
+++ app/Http/Controllers/PessoasController.php
@@ -3,10 +3,18 @@
 namespace App\Http\Controllers;
 
 use App\Pessoa;
+use App\Telefone;
 use Illuminate\Http\Request;
 
 class PessoasController extends Controller
 {
+    private $telefones_controller;
+
+    public function __construct(TelefonesController $telefones_controller)
+    {
+        $this->telefones_controller = $telefones_controller;
+    }
+
     public function index(){
 
         $list_pessoas = Pessoa::all();
@@ -15,4 +23,27 @@ class PessoasController extends Controller
             'pessoas' => $list_pessoas
         ]);
     }
+
+    public function novoView(){
+
+        return view('pessoas.create');
+    }
+
+    public function store(Request $request){
+
+        $pessoa = Pessoa::create($request->all());
+        if ($request->ddd && $request->telefone){
+            $telefone = new Telefone();
+            $telefone->ddd = $request->ddd;
+            $telefone->telefone = $request->telefone;
+            $telefone->pessoa_id = $pessoa->id;
+            $this->telefones_controller->store($telefone);
+
+        }
+            
+        return redirect('/pessoas')->with('message', 'Contato salvo com sucesso!');
+   
+    }
+
+
 }
